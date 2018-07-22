7a3b51c2a0533495ab068200ebcb21a69434cec4
Oxana Khamidova <oxana.khamidova@zalando.de>
2018-07-22 18:51:35 +0200

    add contact fields to official

--- fiim/models.py
+++ fiim/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from ckeditor.fields import RichTextField
 
 
@@ -19,6 +20,13 @@ class Official(models.Model):
     started_at = models.DateField()
     document = models.ForeignKey(to="fiim.Document", on_delete=models.PROTECT)
     published = models.BooleanField(default=False)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    email = models.EmailField(blank=True)
+    vkontakte = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
 
     def __str__(self):
         return f'{self.name} ({self.position})'
