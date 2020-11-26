b9286ac72d52cc43a3d004972de05034e7cf9baa
khamidova <khamidova@users.noreply.github.com>
2020-11-26 16:10:13 +0100

    remove contact form

--- fiim/views.py
+++ fiim/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage
-from django.template.loader import get_template
-from django.contrib import messages
 from .models import DocumentType, Partner, Official
 from .forms import ContactForm
-from mafiafederation.settings import DEFAULT_FROM_EMAIL
 
 
 def documents_list(request):
@@ -24,45 +20,6 @@ def officials_list(request):
 
 def contact(request):
     form_class = ContactForm
-
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            name = request.POST.get('name', '')
-            email = request.POST.get('email', '')
-            subject = request.POST.get('subject', '')
-            message = request.POST.get('message', '')
-
-            # Email the profile with the
-            # contact information
-            template = get_template('fiim/contact_template.txt')
-            context = {
-                'name': name,
-                'email': email,
-                'subject': subject,
-                'message': message,
-            }
-            content = template.render(context)
-            EmailMessage()
-            email = EmailMessage(
-                "Mafiafederation.org: Contact form submission",
-                content,
-                DEFAULT_FROM_EMAIL,
-                [email, DEFAULT_FROM_EMAIL],
-                headers={'Reply-To': email}
-            )
-            try:
-                email.send()
-            except Exception:
-                messages.add_message(request, messages.ERROR,
-                                     'Произошла ошибка при отправке сообщения. Попробуйте позже.')
-            else:
-                messages.add_message(request, messages.SUCCESS, 'Сообщение успешно отправлено.')
-
-            return render(request, 'fiim/contact.html', {
-                'form': form_class,
-            })
 
     return render(request, 'fiim/contact.html', {
         'form': form_class,
